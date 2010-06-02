abf9b76a64ec81e2a223201f33f51abe47f3080c
Adam Crosby <adam@uptill3.com>
2010-06-02 09:26:38 -0400

    Added try/catch and some error handling, as well as docstrings for methods and some other comments.

--- smsstats.py
+++ smsstats.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import time, sqlite3, sys
+import time, sqlite3, sys, os
 from datetime import date, datetime, timedelta
 from optparse import OptionParser
 
@@ -25,14 +25,22 @@ def main():
 	(options, args) = parser.parse_args()
 	if not options.number:
 		print "Missing phone number, option REQUIRED."
+		print "Please provide the '-n' or '--number' argument.  See '-h' for help."
 		sys.exit(2)
 
+	# Collect all the SMS data
 	data = runsearch(options.db_file, options.starttime, options.endtime, options.number, options.chunksize)
+	# Pass SMS raw data to be formatted for the user-specified output
 	output(data, options.output, options.dateformat)
+	# Exit normally
 	sys.exit()
 	
 def output(data, output, dateformat):
-
+	"""Takes a list lists of raw sms data (each sublist is [date, in, out]), 
+	the output option flag passed on the command line (if any),
+	 and the date format string passed on the command line (if any).
+	No return value - prints directly to STDOUT."""
+	
 	if (output == "CSV") or (output =="csv"):
 		# Pretty print to the console
 		printformat = "%s,%s,%s"
@@ -43,11 +51,22 @@ def output(data, output, dateformat):
 		#Default to console
 		printformat = "Date: %s\t\t In: %s\t\tOut:%s"
 
-
-	for row in data:
-		print printformat % (time.strftime(dateformat, row[0]), row[1], row[2])
+	# data is list of lists.  row is [date, in, out] message counts
+	try:
+		for row in data:
+			print printformat % (time.strftime(dateformat, row[0]), row[1], row[2])
+	except TypeError, e:
+			print "Error: Bad date returned, exiting."
+			sys.exit(2)
 
 def runsearch(dbfile, start, end, number, chunksize):
+	"""Method takes the database filename, start date, end date, phone number, and chunksize in seconds to query with.  Returns a list of lists."""
+	if not os.path.exists(dbfile):
+		print "Error: file '%s' does not exist or cannot be found." % dbfile
+		print "Please check the file name and try again."
+		print "See -h for help and all options."
+		sys.exit(2)
+	
 	conn = sqlite3.connect(dbfile)
 	db = conn.cursor()
 	
@@ -57,20 +76,33 @@ def runsearch(dbfile, start, end, number, chunksize):
 
 	# this is convoluted, but it takes the user input "dd-mm-yy" format time stamp, converts it to a unix timestamp.  Python datetime doesn't HAVE a 'totimestamp' capability, 
 	# so you have to convert to a time tuple and use the "time" module's mktime() function instead.
-	startstamp 	= time.mktime(datetime.strptime(start, "%d-%m-%y").timetuple())
-	endstamp 	= time.mktime(datetime.strptime(end, "%d-%m-%y").timetuple())
+	try:
+		startstamp 	= time.mktime(datetime.strptime(start, "%d-%m-%y").timetuple())
+		endstamp 	= time.mktime(datetime.strptime(end, "%d-%m-%y").timetuple())
+	except ValueError, e:
+		print "Error: Date entered in wrong format.  Should be in 'DD-MM-YY' format."
+		print "Please check the date entered and try again."
+		print "See -h for help and all options."
+		sys.exit(2)
 
 	results = []
 	# converting everything to 'int' to keep xrange happy.
-	for x in xrange(int(startstamp), int(endstamp), int(chunksize)):
-		outmsg = db.execute(out_query, (number, x, x+int(chunksize-1)))
-		a = outmsg.fetchall()[0]
-		inmsg = db.execute(in_query, (number, x, x+int(chunksize-1)))
-		b = inmsg.fetchall()[0]
+	try:
+		for x in xrange(int(startstamp), int(endstamp), int(chunksize)):
+			outmsg = db.execute(out_query, (number, x, x+int(chunksize-1)))
+			a = outmsg.fetchall()[0]
+			inmsg = db.execute(in_query, (number, x, x+int(chunksize-1)))
+			b = inmsg.fetchall()[0]
 
-		row = [time.gmtime(x), a[0], b[0]]
-		results.append(row)
-	
+			row = [time.gmtime(x), a[0], b[0]]
+			results.append(row)
+
+	except sqlite3.OperationalError, e:
+		print "Error: Something is wrong or unexpected in the sms database file."
+		print "Are you sure this is a valid, unencrypted iPhone SMS sqlite database?"
+		print "See -h for help and all options."
+		sys.exit(2)
+
 	return results
 
 
